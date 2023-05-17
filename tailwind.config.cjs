14f21cfb7b78ede1b73783f2ece42fbcd50fa60d
Yeon <yeonhwan619@gmail.com>
2023-05-17 18:24:59 +0900

    🎨 Listboards - Listboard pages added edit. tailwind.config - grid-template-listboard add edit. ListView - ListView can accept classNames

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,9 @@ const config = {
         dummy:
           "url('https://cdn.dribbble.com/users/181520/screenshots/9447082/media/ef346873f7ca645f056310305167e101.jpg')",
       },
+      gridTemplateColumns: {
+        listboard: "repeat(auto-fill, minmax(320px, 25%))",
+      },
     },
   },
   plugins: [],
