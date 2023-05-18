49f2a6ab6c142204bae53cd9638c7113b155ff11
Yeon <yeonhwan619@gmail.com>
2023-05-19 01:31:23 +0900

    ⚙️ API/Listboards - return data of getListboards changed

--- src/server/api/routers/listboardsAPI/getListboards.ts
+++ src/server/api/routers/listboardsAPI/getListboards.ts
@@ -1,25 +1,35 @@
+import { z } from "zod";
 import { protectedProcedure } from "../../trpc";
 import { TRPCError } from "@trpc/server";
 
 // verify
 import tokenVerify from "@/server/api/routers/auth/tokenVerify";
 
-const getListboards = protectedProcedure.query(async ({ ctx }) => {
-  const session = ctx.session;
-  const { id } = session.user;
+const getListboards = protectedProcedure
+  .input(
+    z.object({
+      data: z.object({
+        todos: z.optional(z.boolean()),
+      }),
+    })
+  )
+  .query(async ({ ctx, input }) => {
+    const session = ctx.session;
+    const { id } = session.user;
+    const { todos } = input.data;
 
-  if (!tokenVerify(session)) {
-    throw new TRPCError({ message: "TOKEN ERROR", code: "UNAUTHORIZED" });
-  }
+    if (!tokenVerify(session)) {
+      throw new TRPCError({ message: "TOKEN ERROR", code: "UNAUTHORIZED" });
+    }
 
-  const user = await ctx.prisma.user.findUniqueOrThrow({
-    where: { id },
-    select: { listBoards: { include: { todos: true } } },
+    const user = await ctx.prisma.user.findUniqueOrThrow({
+      where: { id },
+      select: { listBoards: { include: { todos } } },
+    });
+
+    const { listBoards } = user;
+
+    return { data: listBoards };
   });
-
-  const { listBoards } = user;
-
-  return { data: listBoards };
-});
 
 export default getListboards;
