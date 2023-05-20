24a6bf193af9070a180dc0de64cb8009bda73579
Yeon <yeonhwan619@gmail.com>
2023-05-20 16:47:22 +0900

    📦 Dependancy - SVGR/Babel (SVG to React Compoenents) edit. next.config.mjs - add webpack configuration for svgr

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
