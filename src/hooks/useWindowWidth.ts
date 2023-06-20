d11774af72034766da0577f8d55f3ed9a790cf75
Yeon <yeonhwan619@gmail.com>
2023-06-20 22:53:54 +0900

    💬 Minor - useWindowWidth initial setState add

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -4,6 +4,8 @@ export const useWindowWidth = (defaultState: boolean, max: number) => {
   const [state, setState] = useState(defaultState);
 
   useEffect(() => {
+    const initialBrowserState = window.innerWidth >= max;
+    setState(initialBrowserState);
     window.addEventListener("resize", (_) => {
       const isMax = window.innerWidth >= max;
 
@@ -11,7 +13,7 @@ export const useWindowWidth = (defaultState: boolean, max: number) => {
       else if (!isMax) setState(false);
       else return;
     });
-  });
+  }, []);
 
   return state;
 };
