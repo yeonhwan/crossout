02590d0a7e0f39a3e1ffc9d9667f6f1b30bd33bf
yeonhwan <yeonhwan619@gmail.com>
2025-06-29 18:59:40 +0900

    🔧 Add cron job vercel functions

---
+++ src/pages/api/cron/stop-freeze.ts
@@ -0,0 +1,65 @@
+export const config = {
+  runtime: "edge",
+};
+
+// every monday 9:30
+const GET = async (request: Request) => {
+  // Auto sign in for stop pausing DB
+  console.log("Trying to auto login with test account for stop DB freezing");
+
+  try {
+    if (!process.env.CRON_SECRET) {
+      throw new Error("Secret is not provided");
+    }
+
+    // validation
+    if (
+      request.headers.get("Authorization") !==
+      `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return new Response("Not Authorized", { status: 401 });
+    }
+
+    // sign in
+    const res = await signin({
+      email: process.env.TEST_EMAIL as string,
+      password: process.env.TEST_PW as string,
+    });
+
+    if (res && res.status >= 200 && res.status < 300) {
+      return new Response("Process completed.");
+    } else {
+      const errorStatus = res?.status || 500;
+      const reason = res?.statusText || "Unknown error occurred.";
+
+      return new Response(`Login failed: ${reason}`, {
+        status: errorStatus,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return new Response("Login failed. Check information and variables", {
+      status: 500,
+    });
+  }
+};
+
+const signin = ({ email, password }: { email: string; password: string }) => {
+  if (!process.env.NEXTAUTH_URL) {
+    throw new Error("URL is not provided.");
+  } else {
+    // from source code
+    return fetch(process.env.NEXTAUTH_URL + "/api/auth/callback/credentials", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+  }
+};
+
+export default GET;
